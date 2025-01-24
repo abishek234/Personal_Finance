e44e91c11a933ccaa8b430ca275d4cfcc537b9e3
abishek <avineshmtp@gmail.com>
2025-01-24 23:55:48 +0530

    route update

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,5 +1,6 @@
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
+import { cors} from "hono/cors";
 
 import accounts from "./accounts";
 import categories from "./categories";
@@ -9,6 +10,14 @@ import transactions from "./transactions";
 export const runtime = "edge";
 
 const app = new Hono().basePath("/api");
+
+app.use('*', cors({
+  origin: 'https://personal-finance-hs8u.onrender.com', // Allow requests from this origin
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow specific HTTP methods
+  headers: ['Content-Type', 'Authorization', 'X-Requested-With'], // Allow specific headers
+}));
+
+app.get('/', (c) => c.json({ message: 'Hello from Hono!' }));
 
 const routes = app
   .route("/accounts", accounts)
