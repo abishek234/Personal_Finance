b2c04d52227ad6159f4e2b27d96bd190caf40fac
abishek234 <100270076+abishek234@users.noreply.github.com>
2025-01-25 00:38:23 +0530

    Update route.ts

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,6 +1,6 @@
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
-import { cors} from "hono/cors";
+import { cors } from 'hono/cors';
 
 import accounts from "./accounts";
 import categories from "./categories";
@@ -11,11 +11,8 @@ export const runtime = "edge";
 
 const app = new Hono().basePath("/api");
 
-app.use(cors({
-  origin: 'https://personal-finance-hs8u.onrender.com', // Allow requests from this origin
-  allowMethods: ['GET', 'POST', 'PUT', 'DELETE'], // Use `allowMethods` instead of `methods`
-  allowHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'], // Allow specific headers
-}));
+app.use(cors());
+
 
 app.get('/', (e: { json: (arg0: { message: string; }) => any; }) => e.json({ message: 'Hello from Hono!' }));
 
